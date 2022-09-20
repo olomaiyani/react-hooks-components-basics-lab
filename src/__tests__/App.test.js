2658e9fdcc3de72450c1416782433e994c3a97bc
Nana <nanaolomaiyani@gmail.com>
2022-09-20 14:41:27 +0300

    completed assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,18 +3,19 @@ import { render, screen } from "@testing-library/react";
 
 import App from "../components/App";
 
-test("renders without errors", () => {
+test("renders the correct child components", () => {
   expect(() => render(<App />)).not.toThrow();
 });
 
 test("renders the correct child components", () => {
   const { container } = render(<App />);
+  
 
   /*
     Uncomment the line below to see the DOM elements being returned
     by the App component in your terminal when you run the tests
   */
-  // screen.debug();
+  //screen.debug();
 
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 function NavBar() {
   return (
     <nav>
@@ -7,7 +6,6 @@ function NavBar() {
     </nav>
   );
 }
-
 function Home() {
   return (
     <div id="home">
@@ -15,18 +13,23 @@ function Home() {
     </div>
   );
 }
-
+function About(){
+  return (
+    <div id = "about">
+      <h2>About</h2>
+    </div>
+  )
+}
 {/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
   );
 }
-
 export default App;
